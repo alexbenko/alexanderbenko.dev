87ebdce69b518c315a6118d99ed03f70055c0caa
Alex Benko <benkoalex521@gmail.com>
2020-09-15 10:35:55 -0700

    Can now pass down styling to my Image component

--- components/Image.jsx
+++ components/Image.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Image = ({file}) =>{
+const Image = ({file,styling={}}) =>{
   return (
     <div className="image-container">
-      <picture>
-        <source srcSet={require(`../images/${file}?webp`)} type="image/webp" />
-        <source srcSet={require(`../images/${file}`)} type="image/jpeg" />
-        <img src={require(`../images/${file}`)} alt={`${file.split('.')[0]}`}/>
+      <picture style={styling}>
+        <source srcSet={require(`../images/${file}?webp`)} style={styling} type="image/webp" />
+        <source srcSet={require(`../images/${file}`)} style={styling} type="image/jpeg" />
+        <img src={require(`../images/${file}`)} style={styling} alt={`${file.split('.')[0]}`}/>
       </picture>
     </div>
   );
