80cc2f5f78b0095e4483f5ddeadbb55a0a9dba69
Alex Benko <benkoalex521@gmail.com>
2020-09-15 11:48:02 -0700

    Switched to hooks

--- layouts/NavItem.js
+++ layouts/NavItem.js
@@ -1,9 +1,8 @@
-import React, { useState } from "react";
+import React, { useSate } from "react";
 import Link from "next/link";
 
-const NavItem = ({endpoint}) =>{
-  const [isSticky, setSticky] = useState(false);
-
+const NavItem = ()=>{
+  const [active,setActive] = useSate(false);
   let style = {};
 
   style.a = {
@@ -13,14 +12,16 @@ const NavItem = ({endpoint}) =>{
   };
 
   style.holder = {
-    backgroundColor: isToggled ? 'purple' : 'green'
+    backgroundColor: this.state.active ? 'purple' : 'green'
   }
 
   return(
-    <div className="navitem-holder" style={style.holder} onClick={toggle}>
-      <Link href={endpoint}><a style={style.a}>{endpoint.substring(1)}</a></Link>
+    <div className="navitem-holder" style={style.holder} onClick={()=>setActive(!active))}>
+      <Link href={this.props.endpoint}><a style={style.a}>{this.props.endpoint.substring(1)}</a></Link>
     </div>
   )
-};
+
+}
+
 
 export default NavItem;
