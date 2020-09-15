218c24122079bba4382601f630824fc20e1326d0
Alex Benko <benkoalex521@gmail.com>
2020-09-15 11:45:42 -0700

    Improved SSR of images

--- layouts/Layout.js
+++ layouts/Layout.js
@@ -1,23 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import Link from "next/link";
 import NavItem from './NavItem';
 
 export default function MyLayout({ children }) {
-  const [counter, setCounter] = useState(0);
   const links = ['/skills','/adventures','/contact'];
 
   return (
     <>
       <div className="nav-bar" style={{display:'flex',flexDirection:'row',justifyContent:'space-evenly'}}>
+        <NavItem endpoint={'/'}/>
         {links.map((link,i) =>{
           return(
             <NavItem endpoint={link} key={i} />
           )
         })}
-
-        <button onClick={() => setCounter(counter + 1)}>
-          Clicked {counter} Times
-        </button>
       </div>
 
       {children}
--- pages/skills.js
+++ pages/skills.js
@@ -1,13 +1,33 @@
 import React from "react";
 import MyLayout from "../layouts/Layout";
+import Image from '../components/Image.jsx';
+import axios from 'axios';
 
-const Skills = () =>{
+const Skills = ({images}) =>{
+  const imgStyle = {
+    verticalAlign: 'middle',
+    width: '50%',
+    height: 'auto',
+
+  }
+
   return(
-    <div className="skills">
-      skills
+    <div className="skills" style={{backgroundImage: "url(" + `${require(`../images/adventures/agate.jpg?trace`).trace}`+ ")"}}>
+      <div className="skill-imgages"  style={{paddingLeft:"10%",display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gridGap: '10%', gridAutoRows: 'minMax(10%, auto)'}}>
+        {images.map((image,i)=>{
+          return(
+            <Image file={'skills/' + image} styling={imgStyle} key={i}/>
+          )})}
+      </div>
     </div>
   )
+}
 
+
+export async function getStaticProps(){
+  const res = await axios.get('http://localhost:4200/api/skills')
+  const images = res.data
+  return { props: {images} }
 }
 
 Skills.Layout = MyLayout;
