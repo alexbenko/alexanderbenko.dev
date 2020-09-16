3c506c9efc5156bb518ee320ec99baec5ed7daa1
Alex Benko <benkoalex521@gmail.com>
2020-09-16 15:02:04 -0700

    Imported css file

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import React from "react";
+import '../css/layout.css';
 
 export default function MyApp({ Component, pageProps }) {
   const Layout = Component.Layout ? Component.Layout : React.Fragment;
