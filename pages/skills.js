7bf4851fa3cd2cad541efebd0aca0140b9eeeaa5
Alex Benko <benkoalex521@gmail.com>
2020-09-16 10:06:13 -0700

    Refractored to a component and successfully renders page

--- pages/skills.js
+++ pages/skills.js
@@ -1,33 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 import MyLayout from "../layouts/Layout";
 import Image from '../components/Image.jsx';
 import axios from 'axios';
 
-const Skills = ({images}) =>{
-  const imgStyle = {
-    verticalAlign: 'middle',
-    width: '50%',
-    height: 'auto',
+class Skills extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      images: []
+    }
 
   }
 
-  return(
-    <div className="skills" style={{backgroundImage: "url(" + `${require(`../images/adventures/agate.jpg?trace`).trace}`+ ")"}}>
-      <div className="skill-imgages"  style={{paddingLeft:"10%",display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gridGap: '10%', gridAutoRows: 'minMax(10%, auto)'}}>
-        {images.map((image,i)=>{
-          return(
-            <Image file={'skills/' + image} styling={imgStyle} key={i}/>
-          )})}
+  componentDidMount(){
+    axios.get('/api/skills')
+    .then((res)=>{
+      this.setState({images:res.data})
+    })
+    .catch(err => console.error(err))
+  }
+
+  render(){
+    const imgStyle = {
+      verticalAlign: 'middle',
+      width: '50%',
+      height: 'auto',
+
+    }
+
+    return(
+      <div className="skills" >
+        <div className="skill-imgages"  style={{paddingLeft:"10%",display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gridGap: '10%', gridAutoRows: 'minMax(10%, auto)'}}>
+          {this.state.images.map((image,i)=>{
+            return(
+              <Image file={'skills/' + image} styling={imgStyle} key={i}/>
+            )})}
+        </div>
       </div>
-    </div>
-  )
-}
+    )
 
+  }
 
-export async function getStaticProps(){
-  const res = await axios.get('http://localhost:4200/api/skills')
-  const images = res.data
-  return { props: {images} }
 }
 
 Skills.Layout = MyLayout;
