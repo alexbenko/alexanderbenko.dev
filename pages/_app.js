7415d7d76ab405dfae91d79bbf060b17a762e822
Alex Benko <benkoalex521@gmail.com>
2020-09-17 10:39:15 -0700

    Set up search engine optimization

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,20 @@
 import React from "react";
-import '../css/layout.css';
+import { NextSeo } from 'next-seo';
 
 export default function MyApp({ Component, pageProps }) {
   const Layout = Component.Layout ? Component.Layout : React.Fragment;
 
   return (
     <Layout>
+      <NextSeo
+        openGraph={{
+          type: 'website',
+          description: "Alexander Benko's Portfolio Website - Made with NextJs",
+          locale: 'en_IE',
+          url: 'https://www.alexanderbenko.dev',
+          site_name: "Alexander Benko-Software Engineer",
+        }}
+      />
       <Component {...pageProps} />
     </Layout>
   )
