e3219a6c065e9b07455fe16a8e375135acf0e9be
Alex Benko <benkoalex521@gmail.com>
2020-09-17 10:55:57 -0700

    Navbar sticks to bottom of the screen and better styling

--- layouts/Layout.js
+++ layouts/Layout.js
@@ -4,23 +4,57 @@ import NavItem from './NavItem';
 import { useRouter } from "next/router";
 
 export default function MyLayout({ children }) {
-  const links = ['/skills','/adventures','/contact'];
-
+  const links = ['/skills','/adventures'];
+  const router = useRouter();
   let style = {};
-
-  style.a = {
-    textTransform: 'capitalize',
+  style.nonActiveButton = {
+    backgroundColor: '#808080',
+    border: 'none',
+    color: 'white',
+    padding: '20px',
+    textAlign: 'center',
     textDecoration: 'none',
-    color: 'black'
+    display: 'block',
+    fontSize: '16px',
+    margin: '4px 2px',
+    cursor: 'pointer',
+    borderRadius:'15%',
+    width:'100%',
+    margin: '10px 0'
+  };
+  style.activeButton = {
+    width:'100%',
+    backgroundColor: 'green',
+    border: 'none',
+    color: 'white',
+    padding: '20px',
+    textAlign: 'center',
+    textDecoration: 'none',
+    display: 'block',
+    fontSize: '16px',
+    cursor: 'pointer',
+    borderRadius:'15%',
+    margin: '10px 0'
   };
 
-  const router = useRouter();
+  style.navbarHolder = {
+    backgroundColor:'#333',
+    overflow: 'hidden',
+    position: 'fixed',
+    bottom: '0',
+    left:'0',
+    width: '100%',
+    display:'flex',
+    flexDirection:'row',
+    justifyContent:'space-evenly',
+    margin:' 0 auto'
+  }
 
   return (
     <>
-      <div className="nav-bar" style={{display:'flex',flexDirection:'row',justifyContent:'space-evenly'}}>
+      <div className="nav-bar" style={style.navbarHolder}>
         <div className={router.pathname == "/" ? "active-link" : "not-link"}>
-          <Link href={'/'}><a>{'Home'}</a></Link>
+          <Link href={'/'}><button style={router.pathname == '/' ? style.activeButton : style.nonActiveButton}><p>{'Home'}</p></button></Link>
         </div>
         {links.map((link,i) =>{
           return(
--- layouts/NavItem.js
+++ layouts/NavItem.js
@@ -2,9 +2,39 @@ import React from "react";
 import Link from "next/link";
 
 const NavItem = ({endpoint,router})=>{
+  let style = {};
+  style.nonActiveButton = {
+    width:'100%',
+    backgroundColor: '#808080',
+    border: 'none',
+    color: 'white',
+    padding: '20px',
+    textAlign: 'center',
+    textDecoration: 'none',
+    display: 'block',
+    fontSize: '16px',
+    margin: '4px 2px',
+    cursor: 'pointer',
+    borderRadius:'15%',
+    margin: '10px 0'
+  };
+  style.activeButton = {
+    width:'100%',
+    backgroundColor: 'green',
+    border: 'none',
+    color: 'white',
+    padding: '20px',
+    textAlign: 'center',
+    textDecoration: 'none',
+    display: 'block',
+    fontSize: '16px',
+    margin: '4px 2px',
+    cursor: 'pointer',
+    borderRadius:'15%'
+  };
   return(
     <div className={router.pathname == endpoint ? "active-link" : "not-link"}>
-      <Link href={endpoint}><a>{endpoint.substring(1)}</a></Link>
+      <Link href={endpoint}><button style={router.pathname == endpoint ? style.activeButton : style.nonActiveButton}><p style={{textTransform: 'capitalize'}}>{endpoint.substring(1)}</p></button></Link>
     </div>
   )
 
