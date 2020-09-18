47432ee5bdf719d3b16c7d1fb7c908f3c6426f03
Alex Benko <benkoalex521@gmail.com>
2020-09-18 13:17:35 -0700

    TESTING OUT icons for navbar

--- layouts/Layout.js
+++ layouts/Layout.js
@@ -1,13 +1,18 @@
-import React, {useState} from "react";
+import React from "react";
 import Link from "next/link";
 import NavItem from './NavItem';
 import { useRouter } from "next/router";
+import { FaRegLifeRing, FaHome } from "react-icons/fa";
+import { IconContext } from "react-icons";
+
 
 export default function MyLayout({ children }) {
+  //make links array an array of objects with the endpoint and the icon
   const links = ['/skills','/adventures'];
   const router = useRouter();
   let style = {};
   style.nonActiveButton = {
+    width:'100%',
     backgroundColor: '#808080',
     border: 'none',
     color: 'white',
@@ -19,7 +24,6 @@ export default function MyLayout({ children }) {
     margin: '4px 2px',
     cursor: 'pointer',
     borderRadius:'15%',
-    width:'100%',
     margin: '10px 0'
   };
   style.activeButton = {
@@ -32,9 +36,9 @@ export default function MyLayout({ children }) {
     textDecoration: 'none',
     display: 'block',
     fontSize: '16px',
+    margin: '4px 2px',
     cursor: 'pointer',
-    borderRadius:'15%',
-    margin: '10px 0'
+    borderRadius:'15%'
   };
 
   style.navbarHolder = {
@@ -47,14 +51,22 @@ export default function MyLayout({ children }) {
     display:'flex',
     flexDirection:'row',
     justifyContent:'space-evenly',
-    margin:' 0 auto'
+    margin:' 0 auto',
+    maxWidth:'100%',
+    zIndex:'9'
   }
 
   return (
     <>
       <div className="nav-bar" style={style.navbarHolder}>
         <div className={router.pathname == "/" ? "active-link" : "not-link"}>
-          <Link href={'/'}><button style={router.pathname == '/' ? style.activeButton : style.nonActiveButton}><p>{'Home'}</p></button></Link>
+          <Link href={'/'}>
+            <button style={router.pathname == '/' ? style.activeButton : style.nonActiveButton}>
+                <IconContext.Provider value={{size:'2.5em'}}>
+                  <FaHome />
+                </IconContext.Provider>
+              </button>
+            </Link>
         </div>
         {links.map((link,i) =>{
           return(
