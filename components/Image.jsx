c90162b7dd201b9c5700586694125da7f167e779
Alex Benko <benkoalex521@gmail.com>
2020-09-25 09:59:23 -0700

    Added support for optinal onclick function

--- components/Image.jsx
+++ components/Image.jsx
@@ -1,16 +1,33 @@
 import React from 'react';
 
-const Image = ({file,styling={}}) =>{
-  return (
-    <div className="image-container">
-      <picture style={styling}>
-        <source srcSet={require(`../images/${file}?webp`)} style={styling} type="image/webp" />
-        <source srcSet={require(`../images/${file}`)} style={styling} type="image/jpeg" />
-        <img src={require(`../images/${file}`)} style={styling} alt={`${file.split('.')[0]}`}/>
-      </picture>
-      <p style={{textTransform: 'capitalize'}}>{`${file.split('.')[0].split('/')[1]}`}</p>
-    </div>
-  );
+//component needs to be passsed a file path, but allows an optinal style object and a function
+const Image = ({file,styling={},func}) =>{
+  if(func === undefined){
+    return (
+      <div className="image-container" >
+        <picture style={styling}>
+          <source srcSet={require(`../images/${file}?webp`)} style={styling} type="image/webp" />
+          <source srcSet={require(`../images/${file}`)} style={styling} type="image/jpeg" />
+          <img src={require(`../images/${file}`)} style={styling} alt={`${file.split('.')[0]}`}/>
+        </picture>
+        <p style={{textTransform: 'capitalize'}}>{`${file.split('.')[0].split('/')[1]}`}</p>
+      </div>
+    );
+
+  } else{
+    return (
+      <div className="image-container" onClick={()=> func(file)}>
+        <picture style={styling}>
+          <source srcSet={require(`../images/${file}?webp`)} style={styling} type="image/webp" />
+          <source srcSet={require(`../images/${file}`)} style={styling} type="image/jpeg" />
+          <img src={require(`../images/${file}`)} style={styling} alt={`${file.split('.')[0]}`}/>
+        </picture>
+        <p style={{textTransform: 'capitalize'}}>{`${file.split('.')[0].split('/')[1]}`}</p>
+      </div>
+    );
+
+  }
+
 
 }
 
