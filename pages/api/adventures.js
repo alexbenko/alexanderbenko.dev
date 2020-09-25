b2fa434c2370845a9b99d7e27cc28c3961e988fa
Alex Benko <benkoalex521@gmail.com>
2020-09-25 09:58:49 -0700

    Retrieves adventure image file paths

---
+++ pages/api/adventures.js
@@ -0,0 +1,32 @@
+import Cors from 'cors'
+import initMiddleware from '../../lib/init-middleware'
+import fs from 'fs';
+
+const cors = initMiddleware(
+  Cors({
+    // Only allow requests with GET
+    methods: ['GET'],
+  })
+)
+
+export default async function handler(req,res){
+  await cors(req, res)
+
+  return new Promise((resolve,reject)=>{
+    fs.readdir('./images/adventures/',(err,images)=>{
+      if(err){
+        console.error(err)
+        res.status(404).json('Error Reading Image Dir')
+        resolve()
+      }
+      //removes .DS_Store from the array if it exists
+      if(images.includes('.DS_Store')){
+        images.shift()
+      }
+
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(images)
+      resolve();
+    })
+  })
+}
