1971c8d3b7894e7a79209556c49774c7379341dc
Alex Benko <benkoalex521@gmail.com>
2020-09-30 10:39:12 -0700

    Improved image component

--- components/Image.jsx
+++ components/Image.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
 //component needs to be passsed a file path, but allows an optinal style object and a function
-const Image = ({file,styling={},func}) =>{
-  if(func === undefined){
-    return (
-      <div className="image-container" >
+const Image = ({file,styling={},func=(file)=>alert(file),name="image-container"}) =>{
+
+  return (
+      <div className='image-container' onClick={()=> func(file)}>
         <picture style={styling}>
           <source srcSet={require(`../images/${file}?webp`)} style={styling} type="image/webp" />
           <source srcSet={require(`../images/${file}`)} style={styling} type="image/jpeg" />
@@ -13,20 +13,6 @@ const Image = ({file,styling={},func}) =>{
         <p style={{textTransform: 'capitalize'}}>{`${file.split('.')[0].split('/')[1]}`}</p>
       </div>
     );
-
-  } else{
-    return (
-      <div className="image-container" onClick={()=> func(file)}>
-        <picture style={styling}>
-          <source srcSet={require(`../images/${file}?webp`)} style={styling} type="image/webp" />
-          <source srcSet={require(`../images/${file}`)} style={styling} type="image/jpeg" />
-          <img src={require(`../images/${file}`)} style={styling} alt={`${file.split('.')[0]}`}/>
-        </picture>
-        <p style={{textTransform: 'capitalize'}}>{`${file.split('.')[0].split('/')[1]}`}</p>
-      </div>
-    );
-
-  }
 
 
 }
