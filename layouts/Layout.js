99a914ae0a4c8e1c909ccd11394603432acd08e4
Alex Benko <benkoalex521@gmail.com>
2020-10-05 16:29:10 -0700

    Started more buttons to bottom navbar

--- layouts/Layout.js
+++ layouts/Layout.js
@@ -1,16 +1,23 @@
 import React from "react";
 import Link from "next/link";
 import NavItem from './NavItem';
+import Contact from '../components/Contact.jsx';
 import { useRouter } from "next/router";
-import { FaRegLifeRing, FaHome, FaGrav } from "react-icons/fa";
+import { FaHome, FaGrav, FaDownload, FaGithub, FaLinkedin } from "react-icons/fa";
 import { IconContext } from "react-icons";
 
 
 export default function MyLayout({ children }) {
-
-  const links = ['/skills','/adventures'];
+  const links = [{endpoint:'/adventures',icon:<FaGrav/>}];
   const router = useRouter();
-  let button = {
+
+  const contacts = [
+    {icon:<FaGithub/>,title:'Github',url:'https://github.com/alexbenko'},
+    {icon:<FaLinkedin/>,title:'Linkedin',url:'https://www.linkedin.com/in/alexander-benko/'}
+  ]
+
+  let style= {}
+  style.button = {
     width:'100%',
     backgroundColor: router.pathname == '/' ? '#32CD32' :'#808080',
     border: 'none',
@@ -26,7 +33,23 @@ export default function MyLayout({ children }) {
     margin: '10px 0'
   };
 
-  let navbarHolder = {
+  style.contactButton = {
+    width:'100%',
+    backgroundColor:'#808080',
+    border: 'none',
+    color: 'white',
+    padding: '20px',
+    textAlign: 'center',
+    textDecoration: 'none',
+    display: 'block',
+    fontSize: '16px',
+    margin: '4px 2px',
+    cursor: 'pointer',
+    borderRadius:'15%',
+    margin: '10px 0',
+  }
+
+  style.navbarHolder = {
     backgroundColor:'#333',
     overflow: 'hidden',
     position: 'fixed',
@@ -35,7 +58,7 @@ export default function MyLayout({ children }) {
     width: '100%',
     display:'flex',
     flexDirection:'row',
-    justifyContent:'space-evenly',
+    justifyContent:'space-around',
     margin:' 0 auto',
     maxWidth:'100%',
     zIndex:'9'
@@ -43,21 +66,29 @@ export default function MyLayout({ children }) {
 
   return (
     <>
-      <div className="nav-bar" style={navbarHolder}>
+      <div className="nav-bar" style={style.navbarHolder}>
         <div className={router.pathname == "/" ? "active-link" : "not-link"}>
           <Link href={'/'}>
-            <button style={button}>
+            <button style={style.button}>
               <IconContext.Provider value={{size:'2.5em'}}>
                 <FaHome />
               </IconContext.Provider>
             </button>
           </Link>
         </div>
-        {links.map((link,i) =>{
-          return(
-            <NavItem router={router} endpoint={link} key={i} />
-          )
-        })}
+
+        {links.map((link,i) => <NavItem router={router} endpoint={link.endpoint} icon={link.icon} key={i} />)}
+
+        <div className="contact-info-buttons" style={{display:'flex',flexDirection:'row',marginLeft:'auto',order: '2'}}>
+          <a href="/benko-resume.pdf" download style={{textDecoration:'none',color:'white'}}>
+            <button style={style.contactButton}>
+              <IconContext.Provider value={{size:'2.5em'}}>
+                <FaDownload />
+              </IconContext.Provider>
+           </button>
+          </a>
+          {contacts.map((item,i)=> <Contact icon={item.icon} title={item.title} url={item.url}/>)}
+        </div>
       </div>
 
       {children}
