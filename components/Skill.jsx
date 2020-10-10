2f88c27f3cc940c9b96333bca5c99ec63ffd3840
Alex Benko <benkoalex521@gmail.com>
2020-10-09 20:29:18 -0700

    Added conditional rendering

--- components/Skill.jsx
+++ components/Skill.jsx
@@ -4,11 +4,11 @@ import { IconContext } from "react-icons";
 const Skill = ({ title, Comp }) =>{
   const [clicked,setClicked] = useState(false);
   return(
-    <div className={`${title}-holder`} onClick={()=>setClicked(!clicked)} style={{cursor:'pointer',textAlign:'center'}}>
-      <IconContext.Provider value={{size:'3em',color:'green'}}>
+    <div className={`${title}-holder`} onClick={()=>setClicked(!clicked)} style={{alignItems:'center'}}>
+      <IconContext.Provider value={{size:'3.5em',color:'#32CD32'}}>
         {Comp}
       </IconContext.Provider>
-      {clicked ? <h1>{title}</h1> : ''}
+      {clicked ? <h2 style={{color:'#32CD32'}}>{title}</h2> : ''}
     </div>
   )
 };
