015376a1ec40b04a1a1a1f82d215c59b4aa065e0
Alex Benko <benkoalex521@gmail.com>
2020-10-09 20:27:20 -0700

    Re did my Image component

--- components/Image.jsx
+++ components/Image.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 
 //component needs to be passsed a file path, but allows an optinal style object and a function
-const Image = ({file,styling={},func=(file)=>alert(file),name="image-container"}) =>{
+const Image = ({ file }) =>{
 
   return (
-      <div className='image-container' onClick={()=> func(file)}>
-        <picture style={styling}>
-          <source srcSet={require(`../images/${file}?webp`)} style={styling} type="image/webp" />
-          <source srcSet={require(`../images/${file}`)} style={styling} type="image/jpeg" />
-          <img src={require(`../images/${file}`)} style={styling} alt={`${file.split('.')[0]}`}/>
-        </picture>
-        <p style={{textTransform: 'capitalize'}}>{`${file.split('.')[0].split('/')[1]}`}</p>
+      <div className='image-container' style={{margin:'0 auto',textAlign:'center'}}>
+        <picture className="welcome-image" >
+            <source className="welcome-image" srcSet={require(`../images/${file}?webp`)} type="image/webp" />
+            <source className="welcome-image" srcSet={require(`../images/${file}?webp`)} type="image/jpeg" />
+            <img className="welcome-image" src={require(`../images/${file}?webp`)} alt={`${file.split('.'[0])}`}/>
+          </picture>
       </div>
     );
 
