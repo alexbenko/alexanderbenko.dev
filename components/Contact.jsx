e4601f11427836f2b348dff337a6b22dd51bcc8c
Alex Benko <benkoalex521@gmail.com>
2020-10-19 23:55:35 -0700

    Cleaned up and changed styling

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -2,27 +2,9 @@ import React,{ useState } from 'react';
 import { IconContext } from "react-icons";
 
 const Contact = ({icon,title,url}) =>{
-  const style = {}
-
-  style.contactButton = {
-    backgroundColor:'#808080',
-    border: 'none',
-    color: 'white',
-    padding: '20px',
-    textAlign: 'center',
-    textDecoration: 'none',
-    display: 'block',
-    fontSize: '16px',
-    margin: '4px 2px',
-    cursor: 'pointer',
-    borderRadius:'15%',
-    margin: '10px 0',
-  }
-
-
   return(
     <div className={`${title}`}>
-      <a target="_blank" rel="noopener noreferrer" href={url} style={{textDecoration:'none',color:'white',fontSize:'22px'}}>
+      <a target="_blank" rel="noopener noreferrer" href={url} style={{textDecoration:'none',color:'rgb(0, 255, 51)',fontSize:'22px'}}>
         {icon}
         <span style={{paddingLeft:'1%'}}>{title}</span>
       </a>
