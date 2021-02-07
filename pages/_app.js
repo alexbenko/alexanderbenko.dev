556ae84a694d04e748ee1ed726c1792f0468da76
Alex Benko <benkoalex521@gmail.com>
2021-02-07 14:45:53 -0800

    Context provider for snacks at root of the omponents

--- pages/_app.js
+++ pages/_app.js
@@ -1,33 +1,42 @@
 import React from "react";
 import { NextSeo } from 'next-seo';
-
+import { SnackbarProvider } from 'notistack';
 import '../public/index.css'
 
 export default function MyApp({ Component, pageProps }) {
   const Layout = Component.Layout ? Component.Layout : React.Fragment;
 
   return (
-    <Layout>
-      <NextSeo
-      title="Alexander Benko"
-      description="The portfolio website for Alexander Benko, a full stack software engineer with a specialty in Full Stack web applications."
-        openGraph={{
-          type: 'website',
-          description: "Alexander Benko,your next Software Engineer",
-          images : [
-            {
-              url: require('../images/me.jpg'),
-              width: 800,
-              height: 600,
-              alt: 'It is me'
-            }
-          ],
-          locale: 'en_IE',
-          url: 'https://www.alexanderbenko.dev',
-          site_name: "Alexander Benko",
-        }}
-      />
-      <Component {...pageProps} />
-    </Layout>
+    <SnackbarProvider
+    anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'right',
+    }}
+    >
+      <Layout>
+        <NextSeo
+        title="Alexander Benko"
+        description="The portfolio website for Alexander Benko, a full stack software engineer with a specialty in Full Stack web applications."
+          openGraph={{
+            type: 'website',
+            description: "Alexander Benko,your next Software Engineer",
+            images : [
+              {
+                url: require('../images/me.jpg'),
+                width: 800,
+                height: 600,
+                alt: 'It is me'
+              }
+            ],
+            locale: 'en_IE',
+            url: 'https://www.alexanderbenko.dev',
+            site_name: "Alexander Benko",
+          }}
+        />
+
+          <Component {...pageProps} />
+
+      </Layout>
+    </SnackbarProvider>
   )
 }
