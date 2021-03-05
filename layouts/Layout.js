c475d3a8506d7cbac2ac0f92f7e0f2aa1b66a9cd
Alex Benko <benkoalex521@gmail.com>
2021-03-05 12:02:40 -0800

    Updated resume

--- layouts/Layout.js
+++ layouts/Layout.js
@@ -49,7 +49,7 @@ export default function MyLayout({ children }) {
 
           <div className="contact-info" style={{display:'flex',flexDirection:'row',position:'absolute',bottom:'0',textAlign:'center',flexWrap:'wrap'}}>
               <div className="resume-container" style={{display:'flex',flexDirection:'row',justifyContent:'space-around'}}>
-                  <a href="/benko-resume.pdf" download style={{textDecoration:'none',color:'rgb(0, 255, 51)',fontSize:'22px'}}>
+                  <a href="/benkoresume.pdf" download style={{textDecoration:'none',color:'rgb(0, 255, 51)',fontSize:'22px'}}>
                     <FaDownload />
                     <span style={{paddingLeft:'1%'}}>Resume</span>
                   </a>
